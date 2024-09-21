278f2a29dd7b65c154cfd9949a8e46d252fd3e69
knsnyo <isc963@naver.com>
2024-09-21 20:36:44 +0900

    [update] route -> routes

--- src/app/(front-end)/_features/index.ts
+++ src/app/(front-end)/_features/index.ts
@@ -1,8 +1,9 @@
 import { Auth } from './Auth'
+import { Common } from './Common'
 import { Feed } from './Feed'
 import { User } from './User'
 import { Write } from './Write'
 
-const Feature = { Auth, Feed, User, Write }
+const Feature = { Auth, Feed, User, Write, Common }
 
 export default Feature
