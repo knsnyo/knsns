b3dba5dc5ae415c312197cfd04c7dfef3babad3e
knsnyo <isc963@naver.com>
2024-10-01 23:08:02 +0900

    [update] edit profile layout

--- src/app/(front-end)/(routes)/(main)/my/page.tsx
+++ src/app/(front-end)/(routes)/(main)/my/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import Shared from 'shared'
-import UserIdPage from '../../(detail)/user/[id]/page'
+import UserIdPage from '../../(detail)/user/[id]/(info)/page'
 
 const Page: React.FC = () => {
 	const id = React.useContext(Shared.Provider.Session)!
