78f854b6be79fe71aa4d00914c23d494603c150a
knsnyo <isc963@naver.com>
2024-10-13 20:53:21 +0900

    [update] bottom navigation

--- src/app/(front-end)/__shared/ui/bottom-navigation/logic.tsx
+++ src/app/(front-end)/__shared/ui/bottom-navigation/logic.tsx
@@ -1,13 +1,11 @@
 import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
-import { Hooks } from '../../hooks'
 import { BOTTOM_NAV_URL, INDEX_MAP, URL_MAP } from './constant'
 
 export const useLogic = () => {
 	const pathname = usePathname()
 	const [index, setIndex] = React.useState(URL_MAP[pathname as BOTTOM_NAV_URL])
 	const router = useRouter()
-	const uid = Hooks.useUid()
 
 	const updateIndex = (event: React.SyntheticEvent, newValue: number) => {
 		setIndex(newValue)
@@ -18,7 +16,6 @@ export const useLogic = () => {
 			case INDEX_MAP.search:
 				return router.push('/search')
 			case INDEX_MAP.my:
-				if (!uid) return
 				return router.push(`/my`)
 			default:
 				break
